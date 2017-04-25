d191f721fbe3f44e84e5d46db4ba040367f84fd8
Vsevolod <Vsevolodloboda@gmail.com>
2017-04-25 23:25:55 +0300

    Добавил класс Programmer

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,8 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-          Cucumber.SayHello();Vsevolod vs = new Vsevolod();
+            Cucumber.SayHello();
+            Vsevolod vs = new Vsevolod();
 
             vs.Say("Git hub это круто, но непонятно");
 
